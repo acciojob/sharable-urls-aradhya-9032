8705464cfdf99fa47f4850ecab07042e45de3c93
aradhya-9032 <120170628+aradhya-9032@users.noreply.github.com>
2023-02-24 12:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+var nameElement = document.quertSelector(#name);
+var yearElement = document.quertSelector(#year);
+var btn = document.quertSelector(#button);
+
+function handleSubmit(event){
+	event.preventDefault();
+	var h3Value = 'https://localhost:8080/';
+	var name = nameElement.value;
+	var year = yearElement.value;
+	if(name && year){
+		h3Value += '?name=' + name + '&year+' + year;
+	}
+	else if (!name && year){
+		h3Value += '?year='+ year;
+	}
+	else if (name && !year){
+		h3Value += '?name='+ name;
+	}
+	h3textContent = h3Value;
+}
+form.addEventListener('submit',handleSubmit)
